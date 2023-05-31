f2a02309b8d5c9f0f408a695823c3059e816c1a6
Mickaël Caro <mickael.caro@sonarsource.com>
2023-05-31 11:20:58 +0200

    remove python code

--- uncompiled/javascript/regex.js
+++
@@ -1,7 +0,0 @@
-function getBitbucketcloudOwner() {
-    var repoUri = 'https://bitbucket.org/mickaelcaro_/sonarcloudtest';
-    var regexp = '\\/(\\w*\\W*)\\/[^\\/]*$';
-    var match = repoUri.match(regexp);
-    console.log(match);
-}
-getBitbucketcloudOwner();
--- uncompiled/javascript/regex.ts
+++
@@ -1,8 +0,0 @@
-function getBitbucketcloudOwner() {
-    let repoUri = 'https://bitbucket.org/mickaelcaro_/sonarcloudtest';
-    let regexp = '\\/(\\w*\\W*)\\/[^\\/]*$';
-    let match = repoUri.match(regexp);
-    console.log(match);
-  }
-
-  getBitbucketcloudOwner();
